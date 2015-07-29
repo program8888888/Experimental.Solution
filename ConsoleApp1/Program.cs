53fdf0b774fce8874b4c5fc7585dd890cb1940a3
program8888888 <program8888888@gmail.com>
2015-07-29 22:58:42 +1000

    j

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,14 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿
+using System;
+using System.Diagnostics;
+using System.ComponentModel;
 
-namespace ConsoleApp1
+namespace MyProcessSample
 {
-    public class Program
+    class MyProcess
     {
-        public void Main(string[] args)
+        public static void Main()
         {
+            Process myProcess = new Process();
+
+            try
+            {
+                myProcess.StartInfo.UseShellExecute = false;
+                // You can start any process, HelloWorld is a do-nothing example.
+                myProcess.StartInfo.FileName = "C:\\HelloWorld.exe";
+                myProcess.StartInfo.CreateNoWindow = true;
+                myProcess.Start();
+                // This code assumes the process you are starting will terminate itself. 
+                // Given that is is started without a window so you cannot terminate it 
+                // on the desktop, it must terminate itself or you can do it programmatically
+                // from this application using the Kill method.
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
---
+++ StartAProcess/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+namespace StartAProcess
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+           
+        }
+
+
+    }
+}
